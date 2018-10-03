3a24f4006205493776da9f17640305fdf7199019
Derrick <derrickwang57@gmail.com>
2018-10-03 15:01:53 +0800

    add a image upload button in trix toolbar

--- app/assets/javascripts/trix_uploads.js
+++ app/assets/javascripts/trix_uploads.js
@@ -4,6 +4,55 @@ Trix.config.attachments.preview.caption = {
   size: false
 };
 
+(function() {
+    // Adding upload button to Trix toolbar on initialization
+    document.addEventListener('trix-initialize', function(e) {
+        trix = e.target;
+        toolBar = trix.toolbarElement;
+
+        // Creation of the button
+        button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("class", "attach");
+        button.setAttribute("data-trix-action", "x-attach");
+        button.setAttribute("title", "Attach a file");
+        button.setAttribute("tabindex", "-1");
+        button.innerText = "图片";
+
+        // Attachment of the button to the toolBar
+        uploadButton = toolBar.querySelector('.trix-button-group.trix-button-group--block-tools').appendChild(button);
+
+        // When the button is clicked
+        uploadButton.addEventListener('click', function() {
+            // Create a temporary file input
+            fileInput = document.createElement("input");
+            fileInput.setAttribute("type", "file");
+            fileInput.setAttribute("multiple", "");
+            // Add listener on change for this file input
+            fileInput.addEventListener("change", function(event) {
+                    var file, _i, _len, _ref, _results;
+                    _ref = this.files;
+                    _results = [];
+                    // Getting files
+                    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                        file = _ref[_i];
+                        // pushing them to Trix
+                        _results.push(trix.editor.insertFile(file));
+                    }
+                    return _results;
+                }),
+                // Then virtually click on it
+                fileInput.click()
+        });
+        return;
+    });
+    document.addEventListener('trix-attachment-remove', function(e) {
+        // file = e.attachment;
+        // Here you could send a request to your app to delete the file
+        window.onbeforeunload = function() {};
+    });
+}).call(this);
+
 function uploadAttachment(attachment) {
   // Create our form data to submit
   var file = attachment.file;
